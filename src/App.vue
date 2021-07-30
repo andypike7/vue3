ee8af99dc388360e9cd53e28b58e6b941b9b0164
Andy Pike <tech@sym.ru>
2021-07-30 12:20:45 +0300

    Vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,26 @@
 <template>
-  <h1>Vue 2</h1>
-  <h2>Welcome!</h2>
+  <v-app>
+    <v-main>
+      <HelloWorld />
+    </v-main>
+  </v-app>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
 
 export default defineComponent({
   name: 'App',
-  components: {},
+
+  components: {
+    HelloWorld,
+  },
+
+  data() {
+    return {
+      //
+    };
+  },
 });
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
