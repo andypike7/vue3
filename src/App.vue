d9e43a0942acc004f3a399a178e51e37e1138f50
Andy Pike <tech@sym.ru>
2021-07-31 10:21:06 +0300

    Clean up

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,37 @@
 <template>
-  <v-app>
+  <v-app class="fill-height">
+    <v-app-bar app dense fixed color="true" class="pl-4 pr-8">
+      <v-app-bar-nav-icon />
+      <v-app-bar-title>Logo</v-app-bar-title>
+      <v-spacer></v-spacer>
+      <v-btn text>Link One</v-btn>
+      <v-btn text>Link Two</v-btn>
+      <v-btn text>Link Three</v-btn>
+    </v-app-bar>
     <v-main>
-      <HelloWorld />
+      <v-container fluid>
+        <hr />
+        <HelloWorld />
+        <hr />
+        ((<SymbolsAsking phrase="phrase" />))
+        <hr />
+      </v-container>
     </v-main>
+    <v-footer app>Copyright &copy; 2021</v-footer>
   </v-app>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import HelloWorld from './components/HelloWorld.vue';
+import SymbolsAsking from './components/SymbolsAsking.vue';
 
 export default defineComponent({
   name: 'App',
 
   components: {
     HelloWorld,
+    SymbolsAsking,
   },
 
   data() {
---
+++ src/components/SymbolsAsking.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>SymbolsAskings: [{{ phrase }}]</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'SymbolsAskings',
+
+  data() {
+    return {};
+  },
+  // props: {
+  //   phrase: null,
+  // },
+  setup() {
+    return {
+      phrase: 'Hello?',
+    };
+  },
+});
+</script>
